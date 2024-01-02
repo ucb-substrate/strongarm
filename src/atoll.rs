481ed12e17ad28705d213f3fb01c33c96bf36753
rohanku <rohankumar@berkeley.edu>
2024-01-02 10:01:32 -0800

    add strongarm layout

---
+++ src/atoll.rs
@@ -0,0 +1,337 @@
+use crate::ClockedDiffComparatorIo;
+use atoll::route::GreedyBfsRouter;
+use atoll::{IoBuilder, Tile, TileBuilder};
+use serde::{Deserialize, Serialize};
+use sky130pdk::atoll::{MosLength, NmosTile, PmosTile, Sky130ViaMaker};
+use sky130pdk::Sky130Pdk;
+use substrate::block::Block;
+use substrate::geometry::align::AlignMode;
+use substrate::geometry::rect::Rect;
+use substrate::io::layout::{Builder, IoShape};
+use substrate::io::schematic::Bundle;
+use substrate::io::{InOut, Input, Io, MosIo, MosIoSchematic, Signal};
+use substrate::layout::{ExportsLayoutData, Layout};
+use substrate::schematic::{CellBuilder, ExportsNestedData, Schematic};
+
+#[derive(Debug, Default, Clone, Io)]
+pub struct TwoFingerMosTileIo {
+    pub sd0: InOut<Signal>,
+    pub sd1: InOut<Signal>,
+    pub sd2: InOut<Signal>,
+    pub g: Input<Signal>,
+    pub b: InOut<Signal>,
+}
+
+impl From<MosIoSchematic> for TwoFingerMosTileIoSchematic {
+    fn from(value: Bundle<MosIo>) -> Self {
+        Self {
+            sd0: value.s,
+            sd1: value.d,
+            sd2: value.s,
+            g: value.g,
+            b: value.b,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Block, Clone, Debug, Hash, PartialEq, Eq)]
+#[substrate(io = "TwoFingerMosTileIo")]
+struct TwoFingerMosTile {
+    w: i64,
+    l: MosLength,
+    kind: MosTileKind,
+}
+
+impl TwoFingerMosTile {
+    pub fn new(w: i64, l: MosLength, kind: MosTileKind) -> Self {
+        Self { w, l, kind }
+    }
+
+    pub fn pmos(w: i64, l: MosLength) -> Self {
+        Self::new(w, l, MosTileKind::Pmos)
+    }
+
+    pub fn nmos(w: i64, l: MosLength) -> Self {
+        Self::new(w, l, MosTileKind::Nmos)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum MosTileKind {
+    Pmos,
+    Nmos,
+}
+
+impl ExportsNestedData for TwoFingerMosTile {
+    type NestedData = ();
+}
+
+impl Schematic<Sky130Pdk> for TwoFingerMosTile {
+    fn schematic(
+        &self,
+        io: &Bundle<<Self as Block>::Io>,
+        cell: &mut CellBuilder<Sky130Pdk>,
+    ) -> substrate::error::Result<Self::NestedData> {
+        cell.flatten();
+        match self.kind {
+            MosTileKind::Pmos => {
+                let pmos = cell.instantiate(PmosTile::new(self.w, self.l, 2));
+                cell.connect(pmos.io().g, io.g);
+                cell.connect(pmos.io().b, io.b);
+                cell.connect(pmos.io().sd[0], io.sd0);
+                cell.connect(pmos.io().sd[1], io.sd1);
+                cell.connect(pmos.io().sd[2], io.sd2);
+            }
+            MosTileKind::Nmos => {
+                let nmos = cell.instantiate(NmosTile::new(self.w, self.l, 2));
+                cell.connect(nmos.io().g, io.g);
+                cell.connect(nmos.io().b, io.b);
+                cell.connect(nmos.io().sd[0], io.sd0);
+                cell.connect(nmos.io().sd[1], io.sd1);
+                cell.connect(nmos.io().sd[2], io.sd2);
+            }
+        }
+        Ok(())
+    }
+}
+
+impl ExportsLayoutData for TwoFingerMosTile {
+    type LayoutData = ();
+}
+
+impl Layout<Sky130Pdk> for TwoFingerMosTile {
+    fn layout(&self, io: &mut Builder<<Self as Block>::Io>, cell: &mut substrate::layout::CellBuilder<Sky130Pdk>) -> substrate::error::Result<Self::LayoutData> {
+        match self.kind {
+            MosTileKind::Pmos => {
+                let pmos = cell.generate(PmosTile::new(self.w, self.l, 2));
+                io.g.merge(pmos.io().g);
+                io.sd0.merge(pmos.io().sd[0].clone());
+                io.sd1.merge(pmos.io().sd[1].clone());
+                io.sd2.merge(pmos.io().sd[2].clone());
+                io.b.merge(pmos.io().b);
+                cell.draw(pmos)?;
+            }
+            MosTileKind::Nmos => {
+                let nmos = cell.generate(NmosTile::new(self.w, self.l, 2));
+                io.g.merge(nmos.io().g);
+                io.sd0.merge(nmos.io().sd[0].clone());
+                io.sd1.merge(nmos.io().sd[1].clone());
+                io.sd2.merge(nmos.io().sd[2].clone());
+                io.b.merge(nmos.io().b);
+                cell.draw(nmos)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Block, Clone, Debug, Hash, PartialEq, Eq)]
+#[substrate(io = "ClockedDiffComparatorIo")]
+pub struct AtollStrongArmInstance {
+    pub half_tail_w: i64,
+    pub input_pair_w: i64,
+    pub inv_nmos_w: i64,
+    pub inv_pmos_w: i64,
+    pub precharge_w: i64,
+}
+
+impl ExportsNestedData for AtollStrongArmInstance {
+    type NestedData = ();
+}
+
+impl ExportsLayoutData for AtollStrongArmInstance {
+    type LayoutData = ();
+}
+
+impl Tile<Sky130Pdk> for AtollStrongArmInstance {
+    fn tile<'a>(
+        &self,
+        io: IoBuilder<'a, Self>,
+        cell: &mut TileBuilder<'a, Sky130Pdk>,
+    ) -> substrate::error::Result<(
+        <Self as ExportsNestedData>::NestedData,
+        <Self as ExportsLayoutData>::LayoutData,
+    )> {
+        let mut ltail =
+            cell.generate_primitive(TwoFingerMosTile::nmos(self.half_tail_w, MosLength::L150));
+        let mut rtail =
+            cell.generate_primitive(TwoFingerMosTile::nmos(self.half_tail_w, MosLength::L150));
+        let mut linput =
+            cell.generate_primitive(TwoFingerMosTile::nmos(self.input_pair_w, MosLength::L150));
+        let mut rinput =
+            cell.generate_primitive(TwoFingerMosTile::nmos(self.input_pair_w, MosLength::L150));
+        let mut linvn =
+            cell.generate_primitive(TwoFingerMosTile::nmos(self.inv_nmos_w, MosLength::L150));
+        let mut rinvn =
+            cell.generate_primitive(TwoFingerMosTile::nmos(self.inv_nmos_w, MosLength::L150));
+        let mut linvp =
+            cell.generate_primitive(TwoFingerMosTile::pmos(self.inv_pmos_w, MosLength::L150));
+        let mut rinvp =
+            cell.generate_primitive(TwoFingerMosTile::pmos(self.inv_pmos_w, MosLength::L150));
+        let mut lprecharge1 =
+            cell.generate_primitive(TwoFingerMosTile::pmos(self.precharge_w, MosLength::L150));
+        let mut rprecharge1 =
+            cell.generate_primitive(TwoFingerMosTile::pmos(self.precharge_w, MosLength::L150));
+        let mut lprecharge2 =
+            cell.generate_primitive(TwoFingerMosTile::pmos(self.precharge_w, MosLength::L150));
+        let mut rprecharge2 =
+            cell.generate_primitive(TwoFingerMosTile::pmos(self.precharge_w, MosLength::L150));
+
+        let mut prev = None;
+
+        for (l, r) in [
+            (&mut ltail, &mut rtail),
+            (&mut linput, &mut rinput),
+            (&mut linvn, &mut rinvn),
+        ] {
+            if let Some(prev) = prev {
+                l.align_rect_mut(prev, AlignMode::Left, 0);
+                l.align_rect_mut(prev, AlignMode::Beneath, 0);
+            }
+
+            r.align_mut(l, AlignMode::Bottom, 0);
+            r.align_mut(l, AlignMode::ToTheRight, 0);
+
+            prev = Some(l.lcm_bounds());
+        }
+
+        for (l, r) in [
+            (&mut linvp, &mut rinvp),
+            (&mut lprecharge1, &mut rprecharge1),
+            (&mut lprecharge2, &mut rprecharge2),
+        ] {
+            l.align_rect_mut(prev.unwrap(), AlignMode::Left, 0);
+            l.align_rect_mut(prev.unwrap(), AlignMode::Beneath, 0);
+            r.align_mut(l, AlignMode::Bottom, 0);
+            r.align_mut(l, AlignMode::ToTheRight, 0);
+
+            prev = Some(l.lcm_bounds());
+        }
+
+        let tail = cell.signal("tail", Signal);
+        let intn = cell.signal("intn", Signal);
+        let intp = cell.signal("intp", Signal);
+
+        for inst in [&ltail, &rtail] {
+            cell.connect(
+                inst.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                    d: tail,
+                    g: io.schematic.clock,
+                    s: io.schematic.vss,
+                    b: io.schematic.vss,
+                }),
+            );
+        }
+
+        cell.connect(
+            linput.io(),
+            Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                d: intn,
+                g: io.schematic.input.p,
+                s: tail,
+                b: io.schematic.vss,
+            }),
+        );
+        cell.connect(
+            rinput.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                d: intp,
+                g: io.schematic.input.n,
+                s: tail,
+                b: io.schematic.vss,
+            }),
+        );
+
+        cell.connect(
+            linvn.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                d: io.schematic.output.n,
+                g: io.schematic.output.p,
+                s: intn,
+                b: io.schematic.vss,
+            }),
+        );
+
+        cell.connect(
+            rinvn.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                d: io.schematic.output.p,
+                g: io.schematic.output.n,
+                s: intp,
+                b: io.schematic.vss,
+            }),
+        );
+
+        cell.connect(
+            linvp.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                d: io.schematic.output.n,
+                g: io.schematic.output.p,
+                s: io.schematic.vdd,
+                b: io.schematic.vdd,
+            }),
+        );
+        cell.connect(
+            rinvp.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                d: io.schematic.output.p,
+                g: io.schematic.output.n,
+                s: io.schematic.vdd,
+                b: io.schematic.vdd,
+            }),
+        );
+
+        cell.connect(
+            lprecharge2.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                d: io.schematic.output.n,
+                g: io.schematic.clock,
+                s: io.schematic.vdd,
+                b: io.schematic.vdd,
+            }),
+        );
+        cell.connect(
+            rprecharge2.io(),
+                Bundle::<TwoFingerMosTileIo>::from(Bundle::<MosIo> {
+                g: io.schematic.clock,
+                b: io.schematic.vdd,
+                s: io.schematic.vdd,
+                d: intp,
+            }),
+        );
+
+        cell.draw(ltail)?;
+        cell.draw(rtail)?;
+        cell.draw(linput)?;
+        cell.draw(rinput)?;
+        cell.draw(linvn)?;
+        cell.draw(rinvn)?;
+        cell.draw(linvp)?;
+        cell.draw(rinvp)?;
+        cell.draw(lprecharge1)?;
+        cell.draw(rprecharge1)?;
+        cell.draw(lprecharge2)?;
+        cell.draw(rprecharge2)?;
+
+        cell.set_top_layer(2);
+        cell.set_router(GreedyBfsRouter);
+        cell.set_via_maker(Sky130ViaMaker);
+
+        // todo: add correct port geometry
+        let tmp = IoShape::with_layers(
+            cell.ctx().layers.li1,
+            Rect::from_sides(-500, -500, -400, -400),
+        );
+
+        io.layout.clock.set_primary(tmp.clone());
+        io.layout.vdd.set_primary(tmp.clone());
+        io.layout.vss.set_primary(tmp.clone());
+        io.layout.input.p.set_primary(tmp.clone());
+        io.layout.input.n.set_primary(tmp.clone());
+        io.layout.output.p.set_primary(tmp.clone());
+        io.layout.output.n.set_primary(tmp);
+
+        Ok(((), ()))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,17 @@ use sky130pdk::corner::Sky130Corner;
 use sky130pdk::mos::MosParams;
 use sky130pdk::mos::{Nfet01v8, Pfet01v8};
 use sky130pdk::Sky130Pdk;
-use spectre::blocks::{Pulse, Vsource};
-use spectre::tran::Tran;
-use spectre::{ErrPreset, Spectre};
+use spectre::Spectre;
 use substrate::block::Block;
 use substrate::context::{Context, PdkContext};
-use substrate::io::TestbenchIo;
-use substrate::io::{
-    DiffPair, DiffPairSchematic, InOut, Input, Io, MosIoSchematic, Node, Output, SchematicType,
-    Signal,
-};
+use substrate::io::schematic::HardwareType;
+use substrate::io::{DiffPair, InOut, Input, Io, MosIoSchematic, Output, Signal};
 use substrate::pdk::corner::Pvt;
-use substrate::schematic::{Cell, CellBuilder, ExportsNestedData, NestedData, Schematic};
-use substrate::simulation::{SimController, SimulationContext, Simulator, Testbench};
+use substrate::schematic::{CellBuilder, ExportsNestedData, NestedData, Schematic};
+use substrate::simulation::{Simulator, Testbench};
+
+pub mod atoll;
+pub mod tb;
 
 #[derive(Debug, Default, Clone, Io)]
 pub struct ClockedDiffComparatorIo {
@@ -32,11 +30,11 @@ pub struct ClockedDiffComparatorIo {
 #[derive(Serialize, Deserialize, Block, Clone, Debug, Hash, PartialEq, Eq)]
 #[substrate(io = "ClockedDiffComparatorIo")]
 pub struct StrongArmInstance {
-    tail: MosParams,
-    input_pair: MosParams,
-    inv_nmos: MosParams,
-    inv_pmos: MosParams,
-    precharge: MosParams,
+    pub tail: MosParams,
+    pub input_pair: MosParams,
+    pub inv_nmos: MosParams,
+    pub inv_pmos: MosParams,
+    pub precharge: MosParams,
 }
 
 impl ExportsNestedData for StrongArmInstance {
@@ -46,7 +44,7 @@ impl ExportsNestedData for StrongArmInstance {
 impl Schematic<Sky130Pdk> for StrongArmInstance {
     fn schematic(
         &self,
-        io: &<<Self as Block>::Io as SchematicType>::Bundle,
+        io: &<<Self as Block>::Io as HardwareType>::Bundle,
         cell: &mut CellBuilder<Sky130Pdk>,
     ) -> substrate::error::Result<Self::NestedData> {
         let tail = cell.signal("tail", Signal);
@@ -161,154 +159,6 @@ impl Schematic<Sky130Pdk> for StrongArmInstance {
     }
 }
 
-#[derive(Serialize, Deserialize, Block, Clone, Debug, Hash, PartialEq, Eq)]
-#[substrate(io = "TestbenchIo")]
-pub struct StrongArmTranTb {
-    dut: StrongArmInstance,
-    vinp: Decimal,
-    vinn: Decimal,
-    pvt: Pvt<Sky130Corner>,
-}
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq, NestedData)]
-pub struct StrongArmTranTbNodes {
-    vop: Node,
-    von: Node,
-    vinn: Node,
-    vinp: Node,
-    clk: Node,
-}
-impl ExportsNestedData for StrongArmTranTb {
-    type NestedData = StrongArmTranTbNodes;
-}
-
-impl Schematic<Spectre> for StrongArmTranTb {
-    fn schematic(
-        &self,
-        io: &<<Self as Block>::Io as SchematicType>::Bundle,
-        cell: &mut CellBuilder<Spectre>,
-    ) -> substrate::error::Result<Self::NestedData> {
-        let dut = cell
-            .sub_builder::<Sky130Pdk>()
-            .instantiate(self.dut.clone());
-
-        let vinp = cell.instantiate(Vsource::dc(self.vinp));
-        let vinn = cell.instantiate(Vsource::dc(self.vinn));
-        let vdd = cell.instantiate(Vsource::dc(self.pvt.voltage));
-        let vclk = cell.instantiate(Vsource::pulse(Pulse {
-            val0: dec!(0),
-            val1: self.pvt.voltage,
-            period: Some(dec!(1000)),
-            width: Some(dec!(100)),
-            delay: Some(dec!(10e-9)),
-            rise: Some(dec!(100e-12)),
-            fall: Some(dec!(100e-12)),
-        }));
-
-        cell.connect(io.vss, vinp.io().n);
-        cell.connect(io.vss, vinn.io().n);
-        cell.connect(io.vss, vdd.io().n);
-        cell.connect(io.vss, vclk.io().n);
-
-        let output = cell.signal("output", DiffPair::default());
-
-        cell.connect(
-            ClockedDiffComparatorIoSchematic {
-                input: DiffPairSchematic {
-                    p: *vinp.io().p,
-                    n: *vinn.io().n,
-                },
-                output: output.clone(),
-                clock: *vclk.io().p,
-                vdd: *vdd.io().p,
-                vss: io.vss,
-            },
-            dut.io(),
-        );
-
-        Ok(StrongArmTranTbNodes {
-            vop: output.p,
-            von: output.n,
-            vinn: *vinn.io().p,
-            vinp: *vinp.io().p,
-            clk: *vclk.io().p,
-        })
-    }
-}
-
-use substrate::simulation::data::{tran, FromSaved, Save, SaveTb};
-
-#[derive(Debug, Clone, Serialize, Deserialize, FromSaved)]
-pub struct ComparatorSim {
-    t: tran::Time,
-    vop: tran::Voltage,
-    von: tran::Voltage,
-    vinn: tran::Voltage,
-    vinp: tran::Voltage,
-    clk: tran::Voltage,
-}
-
-/// The decision made by a comparator.
-#[derive(Copy, Clone, Hash, Eq, PartialEq, Ord, PartialOrd, Debug, Serialize, Deserialize)]
-pub enum ComparatorDecision {
-    /// Negative.
-    ///
-    /// The negative input was larger than the positive input.
-    Neg,
-    /// Positive.
-    ///
-    /// The positive input was larger than the negative input.
-    Pos,
-}
-
-impl SaveTb<Spectre, Tran, ComparatorSim> for StrongArmTranTb {
-    fn save_tb(
-        ctx: &SimulationContext<Spectre>,
-        cell: &Cell<Self>,
-        opts: &mut <Spectre as Simulator>::Options,
-    ) -> <ComparatorSim as FromSaved<Spectre, Tran>>::SavedKey {
-        ComparatorSimSavedKey {
-            t: tran::Time::save(ctx, (), opts),
-            vop: tran::Voltage::save(ctx, cell.data().vop, opts),
-            von: tran::Voltage::save(ctx, cell.data().von, opts),
-            vinn: tran::Voltage::save(ctx, cell.data().vinn, opts),
-            vinp: tran::Voltage::save(ctx, cell.data().vinp, opts),
-            clk: tran::Voltage::save(ctx, cell.data().clk, opts),
-        }
-    }
-}
-
-impl Testbench<Spectre> for StrongArmTranTb {
-    type Output = Option<ComparatorDecision>;
-
-    fn run(&self, sim: SimController<Spectre, Self>) -> Self::Output {
-        let mut opts = spectre::Options::default();
-        sim.set_option(self.pvt.corner, &mut opts);
-        let wav: ComparatorSim = sim
-            .simulate(
-                opts,
-                Tran {
-                    stop: dec!(20e-9),
-                    start: None,
-                    errpreset: Some(ErrPreset::Conservative),
-                },
-            )
-            .expect("failed to run simulation");
-
-        let von = *wav.von.last().unwrap();
-        let vop = *wav.vop.last().unwrap();
-
-        let vdd = self.pvt.voltage.to_f64().unwrap();
-        if abs_diff_eq!(von, 0.0, epsilon = 1e-6) && abs_diff_eq!(vop, vdd, epsilon = 1e-6) {
-            Some(ComparatorDecision::Pos)
-        } else if abs_diff_eq!(von, vdd, epsilon = 1e-6) && abs_diff_eq!(vop, 0.0, epsilon = 1e-6) {
-            Some(ComparatorDecision::Neg)
-        } else {
-            None
-        }
-    }
-}
-
 pub fn sky130_ctx() -> PdkContext<Sky130Pdk> {
     let pdk_root = std::env::var("SKY130_COMMERCIAL_PDK_ROOT")
         .expect("the SKY130_COMMERCIAL_PDK_ROOT environment variable must be set");
@@ -322,6 +172,11 @@ pub fn sky130_ctx() -> PdkContext<Sky130Pdk> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::atoll::AtollStrongArmInstance;
+    use crate::tb::{ComparatorDecision, StrongArmTranTb};
+    use ::atoll::TileWrapper;
+    use sky130pdk::atoll::{MosLength, NmosTile, PmosTile};
+    use std::path::PathBuf;
 
     #[test]
     fn sim_strongarm() {
@@ -372,5 +227,24 @@ mod tests {
             ComparatorDecision::Pos,
             "comparator produced incorrect decision"
         );
+    }
+
+    #[test]
+    fn layout_strongarm() {
+        let work_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/build/layout_strongarm");
+        let gds_path = PathBuf::from(work_dir).join("layout.gds");
+        let ctx = sky130_ctx();
+
+        ctx.write_layout(
+            TileWrapper::new(AtollStrongArmInstance {
+                half_tail_w: 1_250,
+                input_pair_w: 4_000,
+                inv_nmos_w: 2_000,
+                inv_pmos_w: 1_000,
+                precharge_w: 1_000,
+            }),
+            gds_path,
+        )
+        .expect("failed to write layout");
     }
 }
